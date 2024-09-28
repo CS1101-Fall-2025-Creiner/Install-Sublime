0268c3773a0c9427d7726264e5db2e77489d3a31
Alex Creiner <alexbcreiner@gmail.com>
2024-09-28 13:52:25 -0400

    2024-09-28

--- install-sublime-mac.sh
+++ install-sublime-mac.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
 brew install --cask sublime-text
+mkdir -p "~/Library/Application Support/Sublime Text/Installed Packages"
+wget -O "~/Library/Application Support/Sublime Text/Installed Packages/Package Control.sublime-package" https://packagecontrol.io/Package%20Control.sublime-package
 
-mkdir -p "~/.config/sublime-text"
-mkdir -p  "~/.config/sublime-text/Installed Packages/"
-mkdir -p "~/.config/sublime-text/Packages"
+# mkdir -p "~/.config/sublime-text"
+# mkdir -p  "~/.config/sublime-text/Installed Packages/"
+# mkdir -p "~/.config/sublime-text/Packages"
 
-installed_packages_path="$HOME/.config/sublime-text/Installed Packages"
-packages_path="$HOME/.config/sublime-text/Packages"
+# installed_packages_path="$HOME/.config/sublime-text/Installed Packages"
+# packages_path="$HOME/.config/sublime-text/Packages"
 
-for source_file in ./"Installed Packages/"*; do
-    filename=$(basename "$source_file")
-    target_file="${installed_packages_path}/$filename"
+# for source_file in ./"Installed Packages/"*; do
+#     filename=$(basename "$source_file")
+#     target_file="${installed_packages_path}/$filename"
 
-    if [[ -f "$target_file" ]]; then
-        rm "$target_file"
-    fi
-    cp -r "$source_file" "$installed_packages_path"
-done
+#     if [[ -f "$target_file" ]]; then
+#         rm "$target_file"
+#     fi
+#     cp -r "$source_file" "$installed_packages_path"
+# done
 
-for source_file in ./"Packages/"*; do
-    filename=$(basename "$source_file")
-    target_file="${packages_path}/$filename"
+# for source_file in ./"Packages/"*; do
+#     filename=$(basename "$source_file")
+#     target_file="${packages_path}/$filename"
 
-    if [[ -f "$target_file" ]]; then
-        rm "$target_file"
-    fi
-    cp -r "$source_file" "$packages_path"
-done
+#     if [[ -f "$target_file" ]]; then
+#         rm "$target_file"
+#     fi
+#     cp -r "$source_file" "$packages_path"
+# done
