3e75d1403795895c295505ff1727f661e5461703
Alex Creiner <alexbcreiner@gmail.com>
2024-09-28 19:28:52 -0400

    2024-09-28

--- install-sublime-mac.sh
+++ install-sublime-mac.sh
@@ -4,36 +4,4 @@ brew install --cask sublime-text
 #brew install wget
 mkdir -p ~/Library/Application\ Support/Sublime\ Text
 cp -r ./macOS-files/* ~/Library/Application\ Support/Sublime\ Text
-# mkdir -p ~/Library/Application\ Support/Sublime\ Text/Installed\ Packages
-# mkdir -p ~/Library/Application\ Support/Sublime\ Text/Packages/User
-#wget -O ~/Library/Application\ Support/Sublime\ Text/Installed\ Packages/Package\ Control.sublime-package https://packagecontrol.io/Package%20Control.sublime-package
-# cp ./macOS-files/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text/Installed\ Packages
-# cp ./macOS-files/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text/Packages/User
-# cp ./macOS-files/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text/Packages/User
-
-# mkdir -p "~/.config/sublime-text"
-# mkdir -p  "~/.config/sublime-text/Installed Packages/"
-# mkdir -p "~/.config/sublime-text/Packages"
-
-# installed_packages_path="$HOME/.config/sublime-text/Installed Packages"
-# packages_path="$HOME/.config/sublime-text/Packages"
-
-# for source_file in ./"Installed Packages/"*; do
-#     filename=$(basename "$source_file")
-#     target_file="${installed_packages_path}/$filename"
-
-#     if [[ -f "$target_file" ]]; then
-#         rm "$target_file"
-#     fi
-#     cp -r "$source_file" "$installed_packages_path"
-# done
-
-# for source_file in ./"Packages/"*; do
-#     filename=$(basename "$source_file")
-#     target_file="${packages_path}/$filename"
-
-#     if [[ -f "$target_file" ]]; then
-#         rm "$target_file"
-#     fi
-#     cp -r "$source_file" "$packages_path"
-# done
+pip3 install pyright
--- install-sublime-windows.bat
+++ install-sublime-windows.bat
@@ -5,5 +5,6 @@ mkdir "%APPDATA%\Sublime Text"
 
 REM Copy the contents of the windows-files directory to the destination
 xcopy /E /I "windows-files\*" "%APPDATA%\Sublime Text"
+pip3 install pyright
 
 echo Files have been copied successfully!
