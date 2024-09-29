e17083c9754a9f2c3e1e274fd945db693aa8f643
Alex Creiner <alexbcreiner@gmail.com>
2024-09-28 22:19:37 -0400

    2024-09-28

--- install-sublime.sh
+++ install-sublime.sh
@@ -5,29 +5,6 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt update
 sudo apt install sublime-text
 
-mkdir -p "~/.config/sublime-text"
-mkdir -p  "~/.config/sublime-text/Installed Packages/"
-mkdir -p "~/.config/sublime-text/Packages"
-
-installed_packages_path="$HOME/.config/sublime-text/Installed Packages"
-packages_path="$HOME/.config/sublime-text/Packages"
-
-for source_file in ./"Installed Packages/"*; do
-    filename=$(basename "$source_file")
-    target_file="${installed_packages_path}/$filename"
-
-    if [[ -f "$target_file" ]]; then
-        rm "$target_file"
-    fi
-    cp -r "$source_file" "$installed_packages_path"
-done
-
-for source_file in ./"Packages/"*; do
-    filename=$(basename "$source_file")
-    target_file="${packages_path}/$filename"
-
-    if [[ -f "$target_file" ]]; then
-        rm "$target_file"
-    fi
-    cp -r "$source_file" "$packages_path"
-done
+mkdir -p ~/.config/sublime-text
+cp -r ./linux-files/* ~/.config/sublime-text
+pip3 install pyright
