2d92de43492e1879578993288e6d72c99c5e9637
Alex Creiner <alexbcreiner@gmail.com>
2025-09-25 14:37:27 -0400

    2025-09-25

--- install-sublime-windows-helper.bat
+++ install-sublime-windows-helper.bat
@@ -1,10 +1,13 @@
 @echo off
 
-REM Create the destination directory if it doesn't exist
-mkdir "%APPDATA%\Sublime Text"
+REM Create the destination directory if it doesn't exist (no error if it does)
+if not exist "%APPDATA%\Sublime Text" (
+    mkdir "%APPDATA%\Sublime Text"
+)
 
 REM Copy the contents of the windows-files directory to the destination
 xcopy /E /I "windows-files\*" "%APPDATA%\Sublime Text"
+
 pip3 install pyright
 
 echo Files have been copied successfully!
